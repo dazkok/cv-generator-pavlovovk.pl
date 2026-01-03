0e514977da5816b6f522d4110397e88ed75580b9
Pavlo Vovk <dazkok@gmail.com>
2026-01-03 22:52:49 +0100

    extension_loaded('pdo_mysql

--- config/database.php
+++ config/database.php
@@ -59,7 +59,10 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                class_exists('Pdo\Mysql')
+                    ? \Pdo\Mysql::ATTR_SSL_CA
+                    : PDO::MYSQL_ATTR_SSL_CA
+                => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
@@ -79,7 +82,10 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                class_exists('Pdo\Mysql')
+                    ? \Pdo\Mysql::ATTR_SSL_CA
+                    : PDO::MYSQL_ATTR_SSL_CA
+                => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
